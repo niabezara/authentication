bf39e28bb6de14d870a4d8e77b2c961df1003bc9
niabezara <niabezarashvili@yahoo.com>
2023-09-13 16:27:44 +0400

    first commit

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,11 +1,14 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
 
 export async function POST(req: any) {
   try {
     const { name, email, password } = await req.json();
-    console.log(name);
-    console.log(email);
-    console.log(password);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB();
+    await User.create({ name, email, password: hashedPassword });
     return NextResponse.json({ message: "User registered" }, { status: 201 });
   } catch (error) {
     return NextResponse.json({ message: "An error occured" }, { status: 500 });
--- models/user.ts
+++ models/user.ts
@@ -1,2 +1,22 @@
-import mongoose from "mongoose";
-const userSchema = new Schema({});
+import mongoose, { Schema, models } from "mongoose";
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "firstName is required"],
+    },
+
+    email: {
+      type: String,
+      require: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      require: [true, "password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models.User || mongoose.model("User", userSchema);
+export default User;
